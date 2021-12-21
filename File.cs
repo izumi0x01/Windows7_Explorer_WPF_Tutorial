3563f83d4f4b4442a3748f4c0e8fd441c5112ac4
izumi0x01 <izumisohpublic@gmail.com>
2021-12-21 09:29:30 +0900

    データソースの作成

---
+++ File.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Windows7_Explorer_WPF_Tutorial
+{
+    public class File
+    {
+        public string Name { get; set; }
+        public string Type { get; set; }
+        public string Size { get; set; }
+        public string ImageSize { get; set; }
+        public string CreateDate { get; set; }
+    }
+}
